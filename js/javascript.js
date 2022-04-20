68be208a73ba94ea690ae3639fab19f5abba8eb4
Klieckihieohraf <104059253+Klieckihieohraf@users.noreply.github.com>
2022-04-20 11:42:19 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -6,10 +6,16 @@ const osm =
       })
 osm.addTo(map)
 
+function popUPinfo(feature, layer) {
+      layer.bindPopup(feature.properties.NIMI)
+}
+
 async function addDistrictsGeoJson(url) {
       const response = await fetch(url)
       const data = await response.json()
-      const polygons = L.geoJson(data)
+      const polygons = L.geoJson(data {
+                                 onEachFeature: popUPinfo,
+                                 })
       polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
