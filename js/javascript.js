1b3f89666fa8e470c7b3f294e61e35177ce92fc1
Klieckihieohraf <104059253+Klieckihieohraf@users.noreply.github.com>
2022-04-20 11:24:17 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,5 +3,5 @@ const osm =
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
        maxZoom: 19,
        attribution: 'OpenStreetMap contributors',
-})
+      })
 osm.addTo(map)
