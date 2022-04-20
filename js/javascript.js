9ddee1dc86813b929f0a8559c4b6947b84d4b063
Klieckihieohraf <104059253+Klieckihieohraf@users.noreply.github.com>
2022-04-20 11:48:47 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -19,11 +19,11 @@ function popUPinfo(feature, layer) {
 }
 
 async function addDistrictsGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const polygons = L.geoJson(data, {
- onEachFeature: popUPinfo,
- })
- polygons.addTo(map)
+      const response = await fetch(url)
+      const data = await response.json()
+      const polygons = L.geoJson(data, {
+            onEachFeature: popUPinfo,
+      })
+      polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
