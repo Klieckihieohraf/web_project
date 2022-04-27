e9b75de3ec8c025798e4e1db192764f0ec10fb0b
Klieckihieohraf <104059253+Klieckihieohraf@users.noreply.github.com>
2022-04-27 10:46:42 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -6,6 +6,12 @@ const osm =
       })
 osm.addTo(map)
 
+async function addGeoJson(url) {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data.features[0])
+}
+
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 function defaultMapSettings() {
