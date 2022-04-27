d449b9628bb15747dcfadc492bb0437d33eddcaa
Klieckihieohraf <104059253+Klieckihieohraf@users.noreply.github.com>
2022-04-27 10:54:05 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -9,7 +9,18 @@ osm.addTo(map)
 async function addGeoJson(url) {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data.features[0])
+      const heatData = data.features.map(heatDataConvert)
+      const heatMap = L.heatLayer(heatData, { radius: 10 })
+      heatMap.addTo(map)
+}
+
+
+function heatDataConvert(feature) {
+      return [
+            feature.geometry.coordinates[1],
+            feature.geometry.coordinates[0],
+            feature.properties.area,
+      ]
 }
 
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
